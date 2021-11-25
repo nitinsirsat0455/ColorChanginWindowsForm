c57c4c4c567271116b3efa99e4787e0e6754a91b
Nitin Sirsat <nitinsirsat.it@gmail.com>
2021-11-26 01:16:51 +0530

    ColorChange

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorChanginWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public async Task<string> colorChangeMethod()
+        {
+            Random random = new Random();
+            int numberOfRandomColors = 1000;
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= numberOfRandomColors; i++)
+                {
+                    int red = random.Next(0, 255);
+                    int green = random.Next(0, 255);  
+                    int blue = random.Next(0, 255);
+
+                    btnStart.BackColor = Color.FromArgb(red, green, blue);
+                    this.BackColor = Color.FromArgb(red, green, blue);
+                    Thread.Sleep(500);
+                }
+            });
+            return "Task Completed";
+        }
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            var task = await colorChangeMethod();
+        }
+
+    }
+}
